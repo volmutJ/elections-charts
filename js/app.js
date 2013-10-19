66db5ccdee9c9ae8f0b47f3f8e7362e56a1099eb
Pavel Smolka <pavel.smolka@gmail.com>
2013-10-19 17:58:38 +0100

    Debugging Ember directives

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-require('dependencies/jquery-1.9.1');
-require('dependencies/handlebars-runtime-1.0.0');
-require('dependencies/ember-1.0.0');
+//require('dependencies/handlebars-runtime-1.0.0');
+//require('dependencies/ember-1.0.0');
 
 require('dependencies/compiled/templates');
 
 
-require('app/models/*');
+//require('js/models/*');
 //require('app/views/edit_todo_textfield');
 //require('app/views/create_todo_textfield');
 //require('app/controllers/todos_controller');
 //require('app/controllers/filtered_todos_controller');
 //require('app/routes/router');
 
-Elections = Ember.Application.create();
+Elections = Ember.Application.create({
+    LOG_TRANSITIONS: true,
+    LOG_BINDINGS: true,
+    LOG_VIEW_LOOKUPS: true
+});
