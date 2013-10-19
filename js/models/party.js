9efe607da8965296d19dd09d3bd540b37e771d26
Pavel Smolka <pavel.smolka@gmail.com>
2013-10-19 18:57:59 +0100

    Ember Data dependency

--- js/models/party.js
+++ js/models/party.js
@@ -1,10 +1,10 @@
 Elections.Party = DS.Model.extend({
 
-    id:	attr(), // 1	ID strany, začíná od 1, stejné jako vylosované volební číslo
-    name: attr(), //OBČANÉ.CZ	Plný název strany
-    abbr: attr(), //Občané	Zkratka strany
-    votesSum: attr(), //13397	Celkový počet hlasů získaných na celostátní úrovni.
-    votesSumPercent: attr(), //0.0026	Celkový počet procent získaných na celostátní úrovni. Zapsaný jako desetinné číslo (54.3% = 0.543)
+    id:	DS.attr(), // 1	ID strany, začíná od 1, stejné jako vylosované volební číslo
+    name: DS.attr(), //OBČANÉ.CZ	Plný název strany
+    abbr: DS.attr(), //Občané	Zkratka strany
+    votesSum: DS.attr(), //13397	Celkový počet hlasů získaných na celostátní úrovni.
+    votesSumPercent: DS.attr(), //0.0026	Celkový počet procent získaných na celostátní úrovni. Zapsaný jako desetinné číslo (54.3% = 0.543)
 
     fullName: function() {
         return this.get('votesSumPercent') >= 0.05;
